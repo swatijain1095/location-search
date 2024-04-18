75df21a0e10bd91d161117a290c8cfc34ab76b28
Swati Jain <jain.swati71@gmail.com>
2024-04-18 21:47:50 +0200

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import type { Place } from "./api/Place";
+import Map from "./components/Map";
+import LocationSearch from "./components/LocationSearch";
+import { useState } from "react";
+
+function App() {
+  const [place, setPlace] = useState<Place | null>(null);
+
+  return (
+    <div className="h-screen w-screen grid grid-cols-12">
+      <div className="col-span-3 p-2">
+        {place?.name}
+        <LocationSearch onPlaceClick={(p) => setPlace(p)} />
+      </div>
+      <div className="col-span-9">
+        <Map place={place} />
+      </div>
+    </div>
+  );
+}
+
+export default App;
